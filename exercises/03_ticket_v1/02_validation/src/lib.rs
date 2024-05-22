37ba6c38581d93a308e64f401fe38607246095ff
xgh0627 <417644978@qq.com>
2024-05-22 20:58:14 +0800

    fix some code

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -16,8 +16,22 @@ impl Ticket {
     // You'll have to use what you learned in the previous exercises,
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
-    fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+    pub fn new(title: String, description: String, status: String) -> Self {
+        if title.is_empty() {
+            panic!("Title cannot be empty")
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 characters")
+        }
+        if description.is_empty() {
+            panic!("Description cannot be empty")
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 characters")
+        }
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
         Self {
             title,
             description,
@@ -58,7 +72,7 @@ mod tests {
     #[test]
     #[should_panic(expected = "Only `To-Do`, `In Progress`, and `Done` statuses are allowed")]
     fn status_must_be_valid() {
-        Ticket::new(valid_title(), valid_description(), "Funny".into());
+        Ticket::new("To-Do".into(), "In Progress".into(), "Done".into());
     }
 
     #[test]
