59e0774099620c60b27eda2d9bbd532d5a320f55
许国辉 <417644978@qq.com>
2024-05-24 22:30:47 +0800

    keep one day

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -3,8 +3,11 @@
 //  a foreign type (`u32`, from `std`).
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
+use std::cmp::PartialEq;
 
-impl PartialEq for u32 {
+struct Myu32(u32);
+
+impl PartialEq for Myu32 {
     fn eq(&self, _other: &Self) -> bool {
         todo!()
     }
